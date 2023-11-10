07e75c2d2a6ab23bc251a593f72d93e9abfc546a
Nikki Wang <nikkiw011201@gmail.com>
2023-11-10 15:17:44 -0500

    added comments to the js file

--- 05-Bookshelf/script.js
+++ 05-Bookshelf/script.js
@@ -1,7 +1,10 @@
+// created a class with a constructor
 class Bookshelf {
     constructor(books) {
         this.books = books;
     }
+
+    //methods to add, remove, and count books
     addBook(books) {
         this.books.push(books);
         return this.books;
@@ -14,13 +17,20 @@ class Bookshelf {
         return this.books.length;
     }
 }
+
+// creates an empty array books
 var books = [];
+
+// creates an object from the class
 const bookshelf = new Bookshelf(books);
+
+// add and remove methods used
 bookshelf.addBook("The Great Gatsby");
 bookshelf.addBook("To Kill a Mockingbird");
 bookshelf.addBook("Pride and Prejudice");
 bookshelf.removeBook("To Kill a Mockingbird");
 bookshelf.removeBook("The Great Gatsby");
 
+// used the method to count books and console log results
 console.log(bookshelf.getBooks());
 console.log(bookshelf);
